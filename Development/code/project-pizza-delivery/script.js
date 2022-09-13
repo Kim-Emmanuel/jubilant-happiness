41581df7e32552d974a6169dc2cf5f46a0ed4fc8
kim <kimemmanuel004@gmail.com>
2022-09-13 23:04:34 +0300

    Completed project

--- Development/code/project-pizza-delivery/script.js
+++ Development/code/project-pizza-delivery/script.js
@@ -1,3 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        buildToDo(e.target.order.value)
+        form.reset()
+    })
+})
+
+function buildToDo(todo){
+  const p = document.createElement('p');
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.getElementById('list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+  alert('This order will be removed')
+}
+// This code below is for the users' location
+const cords = document.getElementById('cords');
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        cords.innerHTML = "Geolocation is not supported by this browser.";
+
+    }
+}
+
+function showPosition(position) {
+    cords.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+// The nav bar toggle
 $(document).ready(function(){
     $('.menu-toggle').click(function(){
         $('.menu-toggle').toggleClass('active')
@@ -5,12 +46,6 @@ $(document).ready(function(){
     })
 })
 
-const pizzaImg = document.querySelectorAll(".single-menu img");
-for (const pimg of pizzaImg) {
-    pimg.addEventListener('click', e => {
-        console.log("Pizza")        
-    }) 
-}
 
 fetch('https://api.agify.io/?name=Kimkd', {
     headers: {
